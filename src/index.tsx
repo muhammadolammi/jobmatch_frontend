d23432157713399b81ef62e76f4fc17360d62e8c
Muhammad Akewukanwo <muhammadolammi@gmail.com>
2026-02-16 01:54:22 +0100

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 
-const allowedDomain = "gojobmatch.com";
+// const allowedDomain = "gojobmatch.com";
 
 if (
   window.location.hostname.endsWith("web.app") ||

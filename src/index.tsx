822e95546b740157bada3db20d22f407ddfbec1e
Muhammad Akewukanwo <muhammadolammi@gmail.com>
2026-02-16 01:52:08 +0100

    redirect firebase urls

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 
+const allowedDomain = "gojobmatch.com";
+
+if (
+  window.location.hostname.endsWith("web.app") ||
+  window.location.hostname.endsWith("firebaseapp.com")
+) {
+  window.location.replace("https://gojobmatch.com");
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
